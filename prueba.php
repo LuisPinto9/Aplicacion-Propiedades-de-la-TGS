98819ce4d465c7a885c6c1556d7f57b92b5be1e5
LuisPinto9 <91229941+LuisPinto9@users.noreply.github.com>
2022-07-01 09:14:36 -0500

    Se arreglo el php, ya se pueden agregar y obtener los datos de los usuarios

--- prueba.php
+++ prueba.php
@@ -25,7 +25,7 @@
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossorigin="anonymous"></script>
 
-    <script src="./Scripts/scriptRegister.js"></script>
+    <script src="Scripts/scriptGame.js"></script>
     <title>Document</title>
 </head>
 <body>
